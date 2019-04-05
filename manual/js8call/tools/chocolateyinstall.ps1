519d3ff162ae63901d6c824720ecb2178f6fc956
Jeffrey Randow <jrandow3@gatech.edu>
2019-04-04 23:31:31 -0500

    Added JS8Call

---
+++ manual/js8call/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://files.js8call.com/1.0.0/js8call-1.0.0-win32.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'js8call*'
+
+  checksum      = 'B6F9AD3C30452128B6920564165C9BA3D574F5DB2F746850BB41FE2ADAA25830'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ manual/js8call/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+  silentArgs   = '/S '
+  file			= "c:\Program Files (x86)\js8call\Uninstall.exe"
+}
+
+Uninstall-ChocolateyPackage @packageArgs
